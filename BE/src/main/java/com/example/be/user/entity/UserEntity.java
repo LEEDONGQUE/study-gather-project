8b4faf92c5de99dc262452471c04809ed959e350
midori <mahuba@naver.com>
2025-10-01 12:37:23 +0900

    Entity에 대한 파일 작성본입니다

--- BE/src/main/java/com/example/be/user/entity/UserEntity.java
+++ BE/src/main/java/com/example/be/user/entity/UserEntity.java
@@ -1,9 +1,12 @@
 package com.example.be.user.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Table;
+import com.example.be.user.dto.UserDto;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 
 @Entity
 @Table(name="user")
@@ -11,5 +14,28 @@ public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long userId;
+
+    @Column(nullable = false, unique = true){
+        private String studentNumber;
+    }
+    @Column(nullable = false ){
+        private String userName;
+    }
+    @Column(nullable = false, unique = true){
+        private String email;
+    }
+    @Column(nullable = false){
+        private String passWord;
+    }
+    public UserEntity(){}//기본 생성자
+
+    public UserEntity(UserDto userdto){
+        this.studentNumber =userdto.getStudent_number();
+        this.userName = userdto.getUser_name();
+        this.email = userdto.getEmail();
+        this.password = userdto.getPassword();
+    }
+
+
 }
