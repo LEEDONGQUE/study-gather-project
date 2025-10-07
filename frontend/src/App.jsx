855fff6fcf3ee6b887f50de97befdceb12ab4982
kangyujeong7 <jeong4696@gmail.com>
2025-10-08 02:53:19 +0900

    라우터 연결 v1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
-import './App.css'
-import Header from './components/Header';
+import React from "react";
+import "./App.css";
+import { Routes, Route, Outlet } from "react-router-dom";
+import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
-function App() {
+function Layout() {
   return (
-    <div className='app-container'>
-      <Header />
+    <div>
+      <div className="app-container">
+        <Header />
+      </div>
+      <main>
+        <Outlet />
+      </main>
+      <footer>footer</footer>
     </div>
-  )
+  );
 }
 
-export default App;
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="signup" element={<SignupPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
+  );
+}
+
---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,8 @@
+//페이지네이션 있는 파트
+
+export default function HomePage() {
+
+    return(
+        <div>MainPage</div>
+    )
+}
