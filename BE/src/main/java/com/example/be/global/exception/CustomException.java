126184ca24146339c790e3722246dd7125310757
kangyujeong7 <jeong4696@gmail.com>
2025-11-26 07:55:36 +0900

    최종1차에서 주석제거

--- BE/src/main/java/com/example/be/global/exception/CustomException.java
+++
@@ -1,7 +0,0 @@
-package com.example.be.global.exception;
-
-public class CustomException extends RuntimeException {
-    public CustomException(String message) {
-        super(message);
-    }
-}
--- BE/src/main/java/com/example/be/study/entity/ParticipantStatus.java
+++
@@ -1,7 +0,0 @@
-package com.example.be.study.entity;
-
-public enum ParticipantStatus {
-    PENDING,   // 대기
-    ACCEPTED,  // 수락
-    REJECTED   // 거절
-}
